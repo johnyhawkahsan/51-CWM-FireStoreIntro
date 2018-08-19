0f64478f03bfaf6208e4f4c63fa183616b820a82
Muhammad Ahsan <johnyhawkahsan@gmail.com>
2018-08-19 18:48:51 +0500

    Initial Commit-Basic app is setup. Using dependency classpath 'com.google.gms:google-services:3.1.0' in main gradle file. While in app build.gradle file, I'm using appcompat-v7:25.3.1 with firestore 12.0.1 dependencies which is causing trouble.

---
+++ app/src/main/java/com/ahsan/a51_cwm_firestoreintro/IMainActivity.java
@@ -0,0 +1,17 @@
+package com.ahsan.a51_cwm_firestoreintro;
+import com.ahsan.a51_cwm_firestoreintro.models.Note;
+
+/**
+ * Created by User on 5/14/2018.
+ */
+
+public interface IMainActivity {
+
+    void createNewNote(String title, String content);
+
+    void updateNote(Note note);
+
+    void onNoteSelected(Note note);
+
+    void deleteNote(Note note);
+}
